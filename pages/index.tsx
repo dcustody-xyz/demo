5b6ed3aa1113f67560796ef4d8a615cf0da8e927
Batudo <batudo@2pi.network>
2023-10-10 10:58:23 -0300

    Micro tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,18 +7,10 @@ const Home: NextPage = () => {
   const address = useAddress();
 
   return (
-    <main
-      className="min-h-screen min-w-screen bg-cover bg-center bg-no-repeat flex items-center justify-center flex-col pb-12"
-      style={{
-        backgroundImage: `
-            radial-gradient(circle farthest-side at -15% 85%, rgba(90, 122, 255, .36), rgba(0, 0, 0, 0) 42%),
-            radial-gradient(circle farthest-side at 100% 30%, rgba(245, 40, 145, 0.25), rgba(0, 0, 0, 0) 60%)
-          `,
-      }}
-    >
+    <main className="min-h-screen min-w-screen flex items-center justify-center flex-col pb-12">
       <div className="px-2">
         <h1 className="mt-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
-          Web3 Onboarding Template 💅
+          Web3 Onboarding demo
         </h1>
         <p className="text-xl text-muted-foreground mt-6 text-center mb-8">
           A demo flow for onboarding both new and existing users onto a web3
